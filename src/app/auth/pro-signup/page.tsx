b2a0952cb5072db3393965e018b0220e7fb07f08
Benjamin Escobar <bagster@Benjamins-MacBook-Pro.local>
2024-11-30 10:34:17 -0300

    sign up form

---
+++ src/app/auth/pro-signup/page.tsx
@@ -0,0 +1,13 @@
+import StepForm from "@/components/Form/StepForm";
+
+const Page = () => {
+  return (
+  
+    <div className="items-center">
+      <StepForm />
+    </div>
+    
+  );
+};
+
+export default Page;
